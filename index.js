17f348fd3b4059b32472782df29ff68b8842b78f
Girish Sarwal <gsarwal@machydgsarwal.local>
2018-11-29 17:13:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+
+/**
+ * sample to generate a pdf
+ * inputs: 
+ *      html: the html payload
+ *      binary: return the file stream instead of a deeplinked url [*true/false]
+ */
+const sdk = require('kinvey-flex-sdk');
+const PDFDocument = require('pdfkit');
+const ms = require('memory-streams');
+const b64 = require('base-64');
+sdk.service((err, flex) => {
+    if(err){
+        console.log("could not initialize flex!");
+        return;
+    }
+    let f = flex.functions;
+    f.register('generate', function(context, complete, modules){
+        let requestBody = context.body;
+        if(requestBody == null) {
+            return complete().setBody({ "error": "must provide payload to convert"}).badRequest().done();
+        }
+        let doc = new PDFDocument();
+        if(doc == null){
+            return complete().setBody({ "error": "could not instantiate pdf document"}).runtimeError().done();  
+        }
+        
+        /** process the document here  */
+        let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus.  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl.'
+        doc.fontSize(8);
+        doc.text('This text is left aligned. ' + lorem, {
+            width: 410,
+            align: 'left'
+        });
+
+        doc.moveDown();
+        doc.text('This text is left aligned. ' + lorem, {
+            width: 410,
+            align: 'center'
+        });
+        doc.moveDown();
+        doc.text('This text is justified. ' + lorem, {
+            width: 410,
+            align: 'justify'
+        });
+            
+        doc.rect(doc.x, 0, 410, doc.y).stroke();
+        doc.end();
+        /** end processing the document  */
+
+        let responseStream =  new ms.WritableStream();
+        let stream = doc.pipe(responseStream);
+        stream.on('finish', function(){
+            let pdfBytes = stream.toString();
+            if(requestBody.format === undefined){
+                requestBody.format = "raw";
+            }
+            if(requestBody.format === "raw") {
+              return complete().setBody({"PDFBytes": pdfBytes}).ok().done();
+            }
+            else if(requestBody.format === "base64"){
+              var encoded = Buffer.from(pdfBytes).toString('base64')
+              return complete().setBody({"PDFBytes": encoded}).ok().done();
+            } else {
+                return complete().setBody({"error": "the format '" + requestBody.format + "' is unsupported!"}).badRequest().done();
+            }
+
+        });
+    });
+});
+
